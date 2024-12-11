98ed43ef4711518cc08ff93a4a315c49c270aa7b
Thang Nguyen <phuocthang238@gmail.com>
2024-12-11 17:31:40 +0700

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+const message = 'hello anh em'
+const rawHtml = '<span style="color: red">This should be red.</span>'
+const buttonId = 'button-1'
+const buttonClass = 'btn btn-primary'
+const isButtonDisabled = true
+const ojectOffset = {
+  id: 'oject-offset',
+  class: 'btn btn-primary',
+  style: {
+    color: 'blue',
+    fontSize: '20px',
+  },
+}
+</script>
+
+<template>
+  <h1>Syntax</h1>
+  <span>Message: {{ message }}</span>
+  <p>Using text interpolation: {{ rawHtml }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+  <!-- HTML will be rendered with style-->
+  <button :id="buttonId" :class="buttonClass">Click</button>
+  <button :disabled="isButtonDisabled">Button</button>
+  <button v-bind="ojectOffset">click here</button>
+</template>
